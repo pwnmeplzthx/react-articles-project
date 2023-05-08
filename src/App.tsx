ec07924f8965fccbbcd602c4b980df15208c7823
pwnmeplzthx <pshumakov11@mail.ru>
2023-05-08 23:32:07 +0300

    organizing styles, adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { Suspense } from "react"
 import { Link, Route, Routes } from "react-router-dom"
-import './index.scss'
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
-import { Suspense } from "react"
+import './styles/index.scss'
+import { useTheme } from "./theme/useTheme"
 
 type Props = {}
 
 const App = (props: Props) => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
