925e779bee1d67492dc12783e1725d939cabd9fc
pwnmeplzthx <pshumakov11@mail.ru>
2023-05-23 18:27:08 +0300

    memory router react-router (for routes in jest: unit tests)

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('render', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         // чтобы появился toBeInTheDocument установить "@testing-library/jest-dom": "^5.16.2", https://github.com/testing-library/jest-dom#installation
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('toggle sidebar', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleButton = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleButton);
