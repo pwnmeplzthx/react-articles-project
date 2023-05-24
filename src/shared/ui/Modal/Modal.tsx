879d0d4221cbde74fc22a99448a041b2a41ee46a
pwnmeplzthx <pshumakov11@mail.ru>
2023-05-24 22:24:41 +0300

    modal: fixed theme transfer

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -29,7 +29,6 @@ export const Modal = (props: ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     const closeHandler = useCallback(() => {
