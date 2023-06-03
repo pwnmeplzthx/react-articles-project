1a74fb61c941e64a55e000ae9ebb73b3f3b640d8
pwnmeplzthx <pshumakov11@mail.ru>
2023-06-03 21:27:54 +0300

    Private routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,27 +1,41 @@
-import { Suspense } from 'react';
+import { getUserAuthData } from 'entities/User';
+import { Suspense, memo, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
-const AppRouter = () => (
-    <Routes>
-        {Object.values(routeConfig).map((routeItem) => (
-            <Route
-                key={routeItem.path}
-                path={routeItem.path}
-                element={(
-                    // Оборачиваем для lazy-load (подгрузки страниц чанками)
-                    <Suspense fallback={<PageLoader />}>
-                        {/* // Оборачиваем контентную часть, чтобы растянуть ее на остаток (от aside) страницы
-                        // навешиваем на класс page-wrapper: flex-grow (см index.scss) */}
-                        <div className="page-wrapper">
-                            {routeItem.element}
-                        </div>
-                    </Suspense>
-                )}
-            />
-        ))}
-    </Routes>
-);
+const AppRouter = () => {
+    const isAuth = useSelector(getUserAuthData);
 
-export default AppRouter;
+    const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
+        if (route.authOnly && !isAuth) {
+            return false;
+        }
+
+        return true;
+    }), [isAuth]);
+
+    return (
+        <Routes>
+            {routes.map((routeItem) => (
+                <Route
+                    key={routeItem.path}
+                    path={routeItem.path}
+                    element={(
+                        // Оборачиваем для lazy-load (подгрузки страниц чанками)
+                        <Suspense fallback={<PageLoader />}>
+                            {/* // Оборачиваем контентную часть, чтобы растянуть ее на остаток (от aside) страницы
+                            // навешиваем на класс page-wrapper: flex-grow (см index.scss) */}
+                            <div className="page-wrapper">
+                                {routeItem.element}
+                            </div>
+                        </Suspense>
+                    )}
+                />
+            ))}
+        </Routes>
+    );
+};
+
+export default memo(AppRouter);
--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -3,28 +3,29 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
-import { t } from 'i18next';
 
 export interface SidebarItemType {
     path: string;
     text: string;
     Icon: React.VFC<React.SVGProps<SVGSVGElement>>;
+    authOnly?: boolean;
 }
 
 export const SidebarItemsList: SidebarItemType[] = [
     {
         path: RoutePath.main,
         Icon: MainIcon,
-        text: 'Главная',
+        text: 'Main',
     },
     {
         path: RoutePath.about,
         Icon: AboutIcon,
-        text: 'О сайте',
+        text: 'About',
     },
     {
         path: RoutePath.profile,
         Icon: ProfileIcon,
-        text: 'Профиль',
+        text: 'Profile',
+        authOnly: true,
     },
 ];
