ec969c6426a655a167b0d5d1b69aa3e9739d5158
pwnmeplzthx <pshumakov11@mail.ru>
2023-06-25 14:10:24 +0300

    add hook useInfiniteScroll

---
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -0,0 +1,41 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+export interface UseInfiniteScrollOptions {
+    callback?: () => void;
+    triggerRef: MutableRefObject<HTMLElement>;
+    wrapperRef: MutableRefObject<HTMLElement>;
+}
+
+export function useInfiniteScroll({ callback, wrapperRef, triggerRef }: UseInfiniteScrollOptions) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+    const observer = useRef<IntersectionObserver | null>(null);
+
+    useEffect(() => {
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
+
+        if (callback) {
+            const options = {
+                root: wrapperElement,
+                rootMargin: '0px',
+                threshold: 1.0,
+            };
+
+            observer.current = new IntersectionObserver(([entry]) => {
+                // Отрабатывать только когда элемент появляется в зоне видимости
+                if (entry.isIntersecting) {
+                    callback();
+                }
+            }, options);
+
+            observer.current.observe(triggerElement);
+        }
+
+        return () => {
+            if (observer.current && triggerElement) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                observer.current.unobserve(triggerElement);
+            }
+        };
+    }, [callback, triggerRef, wrapperRef]);
+}
