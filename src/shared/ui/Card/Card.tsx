0362f015e3ee50f7cb54f683fff4d47b7cf75e96
pwnmeplzthx <pshumakov11@mail.ru>
2023-06-30 19:56:24 +0300

    styling shared/card component

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -2,18 +2,29 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { HTMLAttributes, memo, ReactNode } from 'react';
 import cls from './Card.module.scss';
 
+export enum CardTheme {
+    NORMAL = 'normal',
+    OUTLINED = 'outlined',
+}
+
 // Добавляем дефолтные пропсы элементы extends HTMLAttributes<HTMLDivElement>
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
     children: ReactNode;
+    theme?: CardTheme;
 }
 
 export const Card = memo((props: CardProps) => {
-    const { className, children, ...otherProps } = props;
+    const {
+        className,
+        children,
+        theme = CardTheme.NORMAL,
+        ...otherProps
+    } = props;
 
     return (
         <div
-            className={classNames(cls.сard, [className])}
+            className={classNames(cls.сard, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
