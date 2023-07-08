5a3c202246ac762f55553e4aa6826319e1e6f237
pwnmeplzthx <pshumakov11@mail.ru>
2023-07-08 17:20:35 +0300

    Headless UI. Listbox

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -1,7 +1,6 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { Select } from 'shared/ui/Select/Select';
 import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ListBox } from 'shared/ui/ListBox/ListBox';
 import { Country } from '../../model/types/country';
 
 interface CountrySelectProps {
@@ -29,13 +28,14 @@ export const CountrySelect = memo(({
     }, [onChange]);
 
     return (
-        <Select
-            className={classNames('', [className])}
-            label={t('Country')}
-            options={options}
-            value={value}
+        <ListBox
             onChange={onChangeHandler}
+            value={value}
+            defaultValue={t('Укажите страну')}
+            label={t('Укажите страну')}
+            items={options}
             readonly={readonly}
+            direction="top"
         />
     );
 });
