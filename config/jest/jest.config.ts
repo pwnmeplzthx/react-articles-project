78c4aacc6f186b909864419f7fb503aa03e6de6f
pwnmeplzthx <pshumakov11@mail.ru>
2023-07-09 12:28:16 +0300

    HTML report for unit (jest) tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -50,7 +50,15 @@ export default {
          */
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
-
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
