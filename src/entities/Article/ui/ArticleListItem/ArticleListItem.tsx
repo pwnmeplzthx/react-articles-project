2f417c1b4b3355f77b9fc0e6b8d2734e51f4aeb8
pwnmeplzthx <pshumakov11@mail.ru>
2023-08-14 00:44:38 +0300

    redesign project pt7

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,104 +1,15 @@
 import { HTMLAttributeAnchorTarget, memo } from 'react';
-import { useTranslation } from 'react-i18next';
-import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { AppLink } from '@/shared/ui/AppLink/AppLink';
-import { Avatar } from '@/shared/ui/Avatar/Avatar';
-import { Button, ButtonTheme } from '@/shared/ui/Button';
-import { Icon } from '@/shared/ui/Icon/Icon';
-import { Text } from '@/shared/ui/Text/Text';
-import { ArticleBlockType, ArticleView } from '../../model/consts/consts';
-import { Article, ArticleTextBlock } from '../../model/types/article';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
-import cls from './ArticleListItem.module.scss';
-import { getRouteArticleDetails } from '@/app/providers/router/config/routeConfig';
-import { AppImage } from '@/shared/ui/redesigned/AppImage';
-import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
-import { Card } from '@/shared/ui/redesigned/Card';
+import { Article } from '../../model/types/article';
+import { ArticleListItemRedesigned } from './ArticleListItemRedesigned/ArticleListItemRedesigned';
+import { ArticleView } from '../../model/consts/consts';
 
-interface ArticleListItemProps {
+export interface ArticleListItemProps {
     className?: string;
     article: Article;
     view: ArticleView;
     target?: HTMLAttributeAnchorTarget;
 }
 
-export const ArticleListItem = memo((props: ArticleListItemProps) => {
-    const {
-        className, article, view, target,
-    } = props;
-    const { t } = useTranslation();
-
-    const types = <Text text={article.type.join(', ')} className={cls.types} />;
-    const views = (
-        <>
-            <Text text={String(article.views)} className={cls.views} />
-            <Icon Svg={EyeIcon} />
-        </>
-    );
-
-    if (view === ArticleView.BIG) {
-        const textBlock = article.blocks.find(
-            (block) => block.type === ArticleBlockType.TEXT,
-        ) as ArticleTextBlock;
-
-        return (
-            <div className={classNames(cls.ArticleListItem, [className, cls[view]])}>
-                <Card className={cls.card}>
-                    <div className={cls.header}>
-                        <Avatar size={30} src={article.user.avatar} />
-                        <Text text={article.user.username} className={cls.username} />
-                        <Text text={article.createdAt} className={cls.date} />
-                    </div>
-                    <Text title={article.title} className={cls.title} />
-                    {types}
-                    <AppImage
-                        fallback={<Skeleton width="100%" height={250} />}
-                        src={article.img}
-                        className={cls.img}
-                        alt={article.title}
-                    />
-                    {textBlock && (
-                        <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
-                    )}
-                    <div className={cls.footer}>
-                        <AppLink
-                            target={target}
-                            to={getRouteArticleDetails(article.id)}
-                        >
-                            <Button theme={ButtonTheme.OUTLINE}>
-                                {t('Читать далее...')}
-                            </Button>
-                        </AppLink>
-                        {views}
-                    </div>
-                </Card>
-            </div>
-        );
-    }
-
-    return (
-        <AppLink
-            target={target}
-            to={getRouteArticleDetails(article.id)}
-            className={classNames(cls.ArticleListItem, [className, cls[view]])}
-        >
-            <Card className={cls.card}>
-                <div className={cls.imageWrapper}>
-                    <AppImage
-                        fallback={<Skeleton width={200} height={200} />}
-                        alt={article.title}
-                        src={article.img}
-                        className={cls.img}
-                    />
-                    <Text text={article.createdAt} className={cls.date} />
-                </div>
-                <div className={cls.infoWrapper}>
-                    {types}
-                    {views}
-                </div>
-                <Text text={article.title} className={cls.title} />
-            </Card>
-        </AppLink>
-    );
-});
+export const ArticleListItem = memo((props: ArticleListItemProps) => (
+    <ArticleListItemRedesigned {...props} />
+));
