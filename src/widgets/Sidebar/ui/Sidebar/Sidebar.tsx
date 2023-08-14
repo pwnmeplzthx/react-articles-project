0ab3fcbc81c697e50291969d9bc2c09e25db74ba
pwnmeplzthx <pshumakov11@mail.ru>
2023-08-14 12:41:35 +0300

    authorization form redesign

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -42,7 +42,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
             )}
         >
             <AppLogo
-                size={collapsed ? 30 : 50}
+                size={collapsed ? 30 : 100}
                 className={cls.appLogo}
             />
             <VStack role="navigation" gap="8" className={cls.items}>
