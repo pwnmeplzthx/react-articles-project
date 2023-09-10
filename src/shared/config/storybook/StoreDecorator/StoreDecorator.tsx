79fe7526299271043966d3505d16ae0aa6772b55
pwnmeplzthx <pshumakov11@mail.ru>
2023-09-10 21:13:04 +0300

    fix storybook config reducers

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,13 +1,16 @@
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
 import { articleDetailsReducer } from '@/entities/Article/model/slice/articleDetailsSlice';
+import { notificationReducer } from '@/entities/Notificaion/model/slice/notificationSlice';
 import { loginReducer } from '@/features/AuthByUsername/model/slice/loginSlice';
 import { addCommentFormReducer } from '@/features/addCommentForm/model/slices/addCommentFormSlice';
 import { profileReducer } from '@/features/editableProfileCard/model/slice/profileSlice';
 import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/model/slices';
 import { articlesPageReducer } from '@/pages/ArticlesPage/model/slices/articlesPageSlice';
+import { usersPageReducer } from '@/pages/UsersPage/model/slices/usersPageSlice';
 import { rtkApi } from '@/shared/api/rtkApi';
 import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { customersPageReducer } from '@/pages/CustomersPage/model/slices/customersPageSlice';
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
@@ -16,6 +19,9 @@ const defaultAsyncReducers: ReducersList = {
     addCommentForm: addCommentFormReducer,
     articleDetailsPage: articleDetailsPageReducer,
     articlesPage: articlesPageReducer,
+    usersPage: usersPageReducer,
+    notification: notificationReducer,
+    customersPage: customersPageReducer,
     [rtkApi.reducerPath]: rtkApi.reducer,
 };
 
